ff7ca08a178431b0a33910e08d5f166038873b45
Daker Fernandes Pinheiro <dakerfp@gmail.com>
2020-12-31 12:37:00 -0300

    minor refactoring

--- ai.h
+++ ai.h
@@ -8,34 +8,6 @@
 
 namespace othello {
 
-class strategy
-{
-private:
-    piece_color color;
-
-protected:
-    virtual pos choose_piece_position(const game &g, const std::vector<pos> &possible_positions) = 0;
-
-public:
-    strategy(piece_color color_=none)
-        : color(color_)
-    {}
-    virtual void reset(piece_color color_) { color = color_; }
-    piece_color player() const { return color; }
-    bool play(game &g) {
-        if (g.player() != player())
-            return false;
-        
-        std::vector<pos> possible_positions = g.possible_place_positions();
-        if (possible_positions.empty())
-            return false;
-
-        pos p = choose_piece_position(g, possible_positions);
-        g.place_piece(p);
-
-        return true;
-    }
-};
 
 class random_strategy : public strategy
 {
@@ -74,29 +46,6 @@ public:
         return random_strategy::choose_piece_position(g, possible_positions);
     }
 };
-
-piece_color play(game &game,
-    const std::unique_ptr<strategy> &strategy_white,
-    const std::unique_ptr<strategy> &strategy_black)
-{
-    while (1) {
-        if (!game.player_can_place_any_piece(game.player())) {
-            if (!game.player_can_place_any_piece(opposite(game.player())))
-                break;
-            game.flip_player();
-        }
-
-        switch (game.player()) {
-        case othello::white:
-            strategy_white->play(game);
-            break;
-        case othello::black:
-            strategy_black->play(game);
-            break;
-        }
-    }
-    return game.winner();
-}
 
 }
 
